e8e2c054c5f01e32dd960e97e50240eced35fa9c
messafi-1337 <essafi0@gmail.comm>
2023-09-13 17:06:18 +0100

    add new function

--- variables/mouhcine.c
+++ variables/mouhcine.c
@@ -1,14 +1,16 @@
 #include<stdbool.h>
 #include<stdio.h>
 
+
 char nom[8] = "mouhcine";
-int age = 21;
+int age;
 float size = 18.5;
 bool a = true;
 bool b = false;
 
 int main(){
-
-	printf("Nom : %s\nage : %d\nsize : %f\na : %d\nb : %d\n", nom, age, size, a, b);
+	printf("Nom : %s\nsize : %f\na : %d\nb : %d\n", nom, size, a, b);
+	printf("age : ");
+	scanf("%d", &age);
 	return 0;
 }

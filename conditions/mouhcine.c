1382d437e61757fccb4177d6ebe1f53e3f7ae699
messafi-1337 <essafi0@gmail.comm>
2023-09-13 17:39:45 +0100

    pushing new file

---
+++ conditions/mouhcine.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main(){
+
+	int num;
+
+	printf("entrer un nombre : \n");
+	scanf("%d", &num);)
+	if (num % 2)
+		printf("ce nombre est pair\n";
+	else (num %! 2)
+		printf("ce nombre est impair\n");
+	return 0;
+}

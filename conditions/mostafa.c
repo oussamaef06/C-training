11f8413da9efb05a89a18257f6efb745bd093f84
MesVortex <meskinemostafa4@gmail.com>
2023-09-14 10:56:53 +0200

    mention!

--- conditions/mostafa.c
+++ conditions/mostafa.c
@@ -8,13 +8,13 @@ int main(){
     scanf("%f", &a);
     if(a < 10)
 	printf("Bonne chance l\'annee prochaine\n");
-    else if(10 <= a < 12)
+    else if(a>=10 && a<12)
         printf("passble!\n");
-    else if(12 <= a < 14)
+    else if(a>=12 && a<14)
 	printf("assez bien!\n");
-    else if(14 <= a < 16)
+    else if(a>=14 && a<16)
         printf("bien!!\n");
-    else if(16 <= a <= 20)
+    else if(a>=16 && a<20)
         printf("tres bien!!!\n");
     else
 	printf("C\'est ne pas une Note >:(\n");

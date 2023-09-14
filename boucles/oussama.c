e07ee7538df936306fb759373e9464d6f81cb895
oussamaef06 <oussamaelfarihi@gmail.com>
2023-09-14 11:41:48 +0100

    boucle oussama

---
+++ boucles/oussama.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main (){
+
+int x,y;
+
+printf("entrer x :");
+scanf("%d",&x);
+
+printf("entrer y :");
+scanf("%d",&y);
+
+if (x>y){
+printf("x est plus grand de y");
+}
+else {
+printf("y est plus grand de x"):
+}
+
+return 0;
+
+}

8866b07bb2c91a36e7013bb50f3eff6f6a856d24
messafi-1337 <essafi0@gmail.comm>
2023-09-15 16:34:44 +0100

    push la reponse d'exercice

---
+++ fonctions/mouhcine.c
@@ -0,0 +1,29 @@
+
+#include<stdio.h>
+
+int ft_min(int a, int b)
+{
+	if (a > b)
+		return (b);
+	return (a);
+}
+
+int ft_max(int a, int b)
+{
+	if (a > b)
+		return (a);
+	return (b);
+}
+
+int main()
+{
+	int t[4];
+
+	printf("entrer 4 nombre :\n");
+	for (int i = 0; i < 4; i++){
+	
+		scanf("%d", &t[i]);
+	}
+	printf("le min est : %d\n", ft_min((ft_min(t[0], t[1])), (ft_min(t[2], t[3]))));
+	printf("le max est : %d\n", ft_max((ft_max(t[0], t[1])), (ft_max(t[2], t[3]))));
+}

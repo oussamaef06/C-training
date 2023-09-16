039f7f1bcd762571a96b5a6a1e33e665a67402ef
oussamaef06 <oussamaelfarihi@gmail.com>
2023-09-16 10:35:12 +0100

    array ex

---
+++ tableaux/Oussama.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main(){
+
+int i;
+float T[5], somme, moyenne;
+
+
+for(i=0;i<5;i++){
+printf("entrer la note d'etudiant %d : " , i+1);
+scanf("%f",&T[i]);
+if (T[i] < 0 || T[i]>20){
+printf("la note est incorrect \n");
+i--;}
+}
+somme = 0;
+
+for(i=0;i<5;i++){
+somme = somme + T[i];
+}
+
+moyenne = somme/5;
+
+printf("la moyenne des notes est : %.2f \n", moyenne);
+
+for(i=0;i<5;i++){
+     printf("la note d\'etudiant n %d: %.2f \n",i+1,T[i]);
+}
+return 0;
+
+}
